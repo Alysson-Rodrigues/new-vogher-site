a06495aa1b830376905106544b40ae5a25986deb
aly <alyrodob@gmail.com>
2023-05-17 03:00:05 -0300

    :bug: Fix
    #

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -13,14 +13,14 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <a href="/">
               <span className="sr-only">{title}</span>
-              <Image className="h-8 w-auto sm:h-10 hover:scale-125 transition duration-500 ease-in-out" src="https://www.vogher.tech/favicon.ico" width={50} height={50} alt="Logo" />
+              <Image className="h-8 w-auto sm:h-10 hover:scale-125 transition duration-500 ease-in-out" src="/logo.png" width={70} height={70} alt="Logo" />
             </a>
           </div>
           <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-            <a href="#" className="text-base font-base-neue-bold uppercase text-slate-300 hover:text-white transition duration-500 ease-in-out">
+            <a href="#" className="text-2xl font-base-neue-bold uppercase text-slate-300 hover:text-white transition duration-500 ease-in-out">
               About
             </a>
-            <a href="#" className="ml-8 text-base font-base-neue-bold uppercase text-slate-300 hover:text-white transition duration-500 ease-in-out">
+            <a href="#" className="ml-8 text-2xl font-base-neue-bold uppercase text-slate-300 hover:text-white transition duration-500 ease-in-out">
               Contact
             </a>
           </div>
