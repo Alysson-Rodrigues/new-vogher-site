34d73fca79dd832762b52c077619e7ad2dbdba82
aly <alyrodob@gmail.com>
2023-05-19 00:55:55 -0300

    :sparkles: Workflow section
    #

--- app/components/Badge/index.tsx
+++ app/components/Badge/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { FaReact, FaNodeJs, FaHtml5, FaCss3Alt } from 'react-icons/fa';
 
-
-// The icons are passed as props to the Badge component like children
 interface ServiceBadgeProps {
     title: string;
     description: string;
@@ -13,12 +11,14 @@ interface ServiceBadgeProps {
     icons?: React.ReactNode;
     className?: string;
     preventDefault?: boolean;
+    imageClassName?: string;
+    textColor?: string;
 }
 
 
-function Badge({ title, description, imageUrl, badgeBackground, className, onMouseOver, onMouseOut, icons, preventDefault }: ServiceBadgeProps) {
+function Badge({ title, description, imageUrl, badgeBackground, className, onMouseOver, onMouseOut, icons, preventDefault, imageClassName,textColor }: ServiceBadgeProps) {
     return (
-        <div className={`max-w-xs badge bg-[${badgeBackground}] ${className} shadow-lg rounded-lg overflow-hidden`} onMouseOver={
+        <div className={`badge bg-[${badgeBackground}] ${className} shadow-lg rounded-lg overflow-hidden`} onMouseOver={
             onMouseOver ? onMouseOver : (event) => {
                 if (!preventDefault){
                 event.currentTarget.style.transition = 'transform 0.5s ease';
@@ -35,10 +35,10 @@ function Badge({ title, description, imageUrl, badgeBackground, className, onMou
             }
         }
         >
-            <div className="h-40 bg-cover bg-center" style={{ backgroundImage: `url(${imageUrl})` }}></div>
+            <div className={`h-40 bg-cover bg-center ${imageClassName}`} style={{ backgroundImage: `url(${imageUrl})` }}></div>
             <div className="p-4">
-                <h3 className="font-bold text-xl text-black font-base-neue-bold mb-2">{title}</h3>
-                <p className="text-black font-base-neue-regular mb-4">{description}</p>
+                <h3 className={`font-bold text-xl text-[${textColor}] font-base-neue-bold mb-2`}>{title}</h3>
+                <p className={`font-base-neue-regular mb-4 text-[${textColor}]`}>{description}</p>
                 <div className="flex flex-wrap justify-left items-center gap-2">
                     {icons}
                 </div>
